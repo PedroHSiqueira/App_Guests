3351088e749f2db2fa3ee90d6f0561aeeb6d531f
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-02-15 17:55:35 -0300

    Add a Repository Class

--- app/src/main/java/com/example/guests/view/PresentsFragment.kt
+++ app/src/main/java/com/example/guests/view/PresentsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.guests.ui
+package com.example.guests.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.guests.databinding.FragmentPresentsBinding
+import com.example.guests.viewModel.PresentsViewModel
 
 class PresentsFragment : Fragment() {
 
